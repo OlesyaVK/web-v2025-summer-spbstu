0cd4e6d1569f1a320a0e64ee4276d173d885da84
SofiyLop <lopatina.sa@edu.spbstu.ru>
2025-07-12 15:13:28 +0300

    [TASK-4] Remove and add languages to the temporary list

--- sofia.lopatina/task-4/programmer.js
+++ sofia.lopatina/task-4/programmer.js
@@ -40,29 +40,65 @@ console.log(test.languages);
 console.log(test.languageCount);
 
 //work with interface
-let button_add_programmer = document.querySelector('#add_programmer');
-//button.addEventListener('click', createProgrammer();
-
-//function createProgrammer(name, languages) {
-//    let programmer = new Programmer()
-//}
+//Временные значения формы создания программиста
 let language_list = [];
 
+let button_add_programmer = document.querySelector('#add_programmer');
 let button_add_language = document.querySelector('#add_language');
+let button_remove_language = document.querySelector('#remove_language');
+
+button_add_programmer.addEventListener('click', function(event) {
+    event.preventDefault(); // Предотвращаем отправку формы
+    createProgrammer();
+});
 button_add_language.addEventListener('click', function(event) {
-    event.preventDefault(); // Предотвращает отправку формы
+    event.preventDefault(); // Предотвращаем отправку формы
     addLanguageToList();
+});
+button_remove_language.addEventListener('click', function(event) {
+    event.preventDefault(); // Предотвращаем отправку формы
+    removeLanguageFromList();
 });
 
 function addLanguageToList() {
     let input_language = document.querySelector('#input_language').value.trim();
     if (input_language != "" && language_list.indexOf(input_language) == -1) {
         language_list.push(input_language);
-        let li = document.createElement('li');
-        li.innerHTML = input_language;
         let ol = document.querySelector('#language_list');
-        ol.append(li);
+        ol.textContent = '';
+        for (let language of language_list) {
+            let li = document.createElement('li');
+            li.innerHTML = language;
+            ol.append(li);
+        }
+        let language_counter = document.querySelector('#language_counter');
+        language_counter.value = language_list.length;
         console.log(language_list);
     }
 }
-
+function removeLanguageFromList() {
+    let input_language = document.querySelector('#input_language').value.trim();
+    if (input_language != "" && language_list.indexOf(input_language) != -1) {
+        language_list.splice(language_list.indexOf(input_language), 1);
+        let ol = document.querySelector('#language_list');
+        ol.textContent = '';
+        for (let language of language_list) {
+            let li = document.createElement('li');
+            li.innerHTML = language;
+            ol.append(li);
+        }
+        let language_counter = document.querySelector('#language_counter');
+        language_counter.value = language_list.length;
+        console.log(language_list);
+    }
+}
+function createProgrammer() {
+    let input_name = document.querySelector('#input_name').value.trim();
+    if (input_name == "") {
+        console.log("The programmer's name was not entered.");
+    } else if (language_list.length == 0) {
+        console.log("The programmer has no languages specified.");
+    } else {
+        console.log("Ok");
+    }
+}
